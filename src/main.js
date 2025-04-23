286c79784bd602daec81ad9748898fcf3ef18e74
René Elizalde <reroes799@gmail.com>
2025-04-22 23:13:13 -0500

    Proyecto Vite inicial

---
+++ src/main.js
@@ -0,0 +1,7 @@
+setupCounter(document.querySelector('#counter'))
+import $ from 'jquery';
+import dt from 'datatables.net';
+
+$(document).ready(function () {
+  $('#tabla-ejemplo').DataTable();
+});
