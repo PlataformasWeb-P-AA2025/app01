063adbe60540be3d2a92ff1fa46244ee3a364f70
René Elizalde <reroes799@gmail.com>
2025-04-22 23:33:16 -0500

    ejemplo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-setupCounter(document.querySelector('#counter'))
 import $ from 'jquery';
 import dt from 'datatables.net';
+import 'datatables.net-dt';
 
 $(document).ready(function () {
   $('#tabla-ejemplo').DataTable();
 });
+
